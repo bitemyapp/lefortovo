f3856b3c33b4036093200c0da6055024d65550ed
Chris Allen <cma@bitemyapp.com>
2017-10-15 15:38:22 -0500

    Add makefile support

--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,22 @@ use futures::{Future, Stream};
 
 use std::str;
 
-fn build_url(pl: &str) -> String {
+fn build_gitignore_url(pl: &str) -> String {
     format!("https://raw.githubusercontent.com/github/gitignore/master/{}.gitignore",
             pl)
 }
 
-fn get_from_github(lang: &str) {
+fn build_makefile_url(pl: &str) -> String {
+    format!("https://raw.githubusercontent.com/bitemyapp/makefiles/master/{}.makefile",
+            pl)
+}
+
+fn get_from_github(url: hyper::Uri) {
     let mut core = tokio_core::reactor::Core::new().unwrap();
     let handle = core.handle();
     let client = hyper::Client::configure()
         .connector(hyper_tls::HttpsConnector::new(4, &handle).unwrap())
         .build(&handle);
-
-    let url = build_url(lang).parse().unwrap();
     let mut s = String::new();
     {
         let work = client.get(url).and_then(|res| {
@@ -40,6 +43,20 @@ fn main() {
         .version("1.0")
         .author("Chris Allen <cma@bitemyapp.com>")
         .about("For putting things in a hole where they are forgotten")
+        .arg(Arg::with_name("gitignore")
+             .short("g")
+             .long("gitignore")
+             .value_name("gitignore")
+             .help("Fetch a gitignore file from github/gitignore")
+             .conflicts_with("makefile")
+             .takes_value(false))
+        .arg(Arg::with_name("makefile")
+             .short("m")
+             .long("makefile")
+             .value_name("makefile")
+             .help("Fetch a makefile from bitemyapp/makefiles")
+             .conflicts_with("gitignore")
+             .takes_value(false))
         .arg(Arg::with_name("lang")
             .short("l")
             .long("lang")
@@ -49,5 +66,15 @@ fn main() {
             .takes_value(true))
         .get_matches();
     let lang = matches.value_of("lang").unwrap();
-    get_from_github(lang);
+    let gitignore = matches.is_present("gitignore");
+    let makefile = matches.is_present("makefile");
+
+    let url = match (gitignore, makefile) {
+        (true, true) => panic!("You must pick gitignore or makefile, not both!"),
+        (true, false) => build_gitignore_url(lang).parse().unwrap(),
+        (false, true) => build_makefile_url(lang).parse().unwrap(),
+        (false, false) => build_gitignore_url(lang).parse().unwrap(),
+    };
+
+    get_from_github(url);
 }
